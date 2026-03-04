b1c8cfe63101a9971c47b621442ce86413461660
Araújo <0000112025677xsp@al.educacao.sp.gov.br>
2026-03-04 15:53:17 -0300

    feat: add javascript test

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("title")
+const subitle = document.getElementById("subtitle")
+const buttom = document.getElementById('mybuttom')
+
+function changeColor() {
+    document.body.style.backgroundColor = "gray"
+    title.style.color = "black"
+    subitle.style.color = "black"
+    buttom.style.backgroundColor = 'gray'
+}
